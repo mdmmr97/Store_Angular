b7262eaa0c84174cf06b6f83c4344d7031e12b16
mdmmr97 <mdmmr97@hotmail.com>
2023-05-16 08:32:45 +0200

    Caso Practico 12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoryListComponent } from './entities/category/category-list/category-list.component';
+import { ItemListComponent } from './entities/item/item-list/item-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'items', component: ItemListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { CategoryListComponent } from './entities/category/category-list/category-list.component';
+import { CategoryFormComponent } from './entities/category/category-form/category-form.component';
+import { ItemListComponent } from './entities/item/item-list/item-list.component';
+import { ItemFormComponent } from './entities/item/item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoryListComponent,
+    CategoryFormComponent,
+    ItemListComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule,
