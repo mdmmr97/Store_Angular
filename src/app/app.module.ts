e89a7f9aaaa39e79ed40f19fdd709af8e2324489
mdmmr97 <mdmmr97@hotmail.com>
2023-05-18 20:56:05 +0200

    Caso Practico 26

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
@@ -27,6 +28,7 @@ import { HttpRequestIntercept } from './config/interceptors/http-request-interce
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule
